c878833b4d03633b83f5fd3e4b9cfe2e764d3403
danielwalke <daniel.walke@ovgu.de>
2025-11-19 07:25:47 +0100

    seed init for reproducibility

--- gnn_evaluation/SBC.py
+++ gnn_evaluation/SBC.py
@@ -1,15 +1,10 @@
-import torch
-import pandas as pd
-from torch_geometric.data import Data
-from torch_geometric.loader import NeighborLoader
-from torch.nn import functional as F
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler
-from constants.feature_names import FEATURES
 from gnn_evaluation.model.GAT_Model import Model
 from gnn_evaluation.model.ModelWrapper import ModelWrapperClassifications
 from sklearn.metrics import roc_auc_score
 from gnn_evaluation.graphloader.Sbc_Graphloader import GraphLoaderSBC
+from gnn_evaluation.utils.SeedInitialization import initialize_seed
+import torch
+from torch.nn import functional as F
 
 if __name__ == "__main__":
     BATCH_SIZE = 512
@@ -17,6 +12,9 @@ if __name__ == "__main__":
     HIDDEN_DIM = 128
     HEADS = 8
     DROPOUT = 0.0
+    
+    initialize_seed(42)
+        
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     sbc_graphloader = GraphLoaderSBC()
     sbc_graphloader.initialize(BATCH_SIZE)
@@ -29,7 +27,7 @@ if __name__ == "__main__":
     pos_weight = (train_labels == 0).sum() / (train_labels == 1).sum()
     criterion = torch.nn.BCEWithLogitsLoss(pos_weight=torch.tensor([pos_weight], dtype=torch.float, device=device))
         
-    model_wrapper = ModelWrapperClassifications(model=model, model_name="GAT_SBC", epochs=5000, patience=5, pred_proba_based_metric=True, minimize_metric=False, eval_metric=roc_auc_score)
+    model_wrapper = ModelWrapperClassifications(model=model, model_name="GAT_SBC", epochs=2, patience=5, pred_proba_based_metric=True, minimize_metric=False, eval_metric=roc_auc_score)
     model_wrapper.train(sbc_graphloader.train_loader, sbc_graphloader.val_loader, optimizer, criterion, device)
     test_auc = model_wrapper.evaluate(sbc_graphloader.test_loader, device)
     print(f"Test AUC: {test_auc:.4f}")
