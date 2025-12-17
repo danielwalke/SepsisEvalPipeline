a12a5517f65298bd832dbe2e1be6b1cbc6651456
danielwalke <daniel.walke@ovgu.de>
2025-12-17 09:02:18 +0100

    feature prefixing

--- 1_preprocess/utils/CountFunctions.py
+++ 1_preprocess/utils/CountFunctions.py
@@ -1,11 +1,11 @@
 def count_cbc_cases(data):
-    comp_data = data.query("~(WBC.isnull() & HGB.isnull() & MCV.isnull() & PLT.isnull() & RBC.isnull())",
+    comp_data = data.query("~(f__WBC.isnull() & f__HGB.isnull() & f__MCV.isnull() & f__PLT.isnull() & f__RBC.isnull())",
                            engine='python')
     unique_data = comp_data.drop_duplicates(subset=["Id", "Center"])
     return len(unique_data)
 
 
 def count_cbc(data):
-    comp_data = data.query("~(WBC.isnull() & HGB.isnull() & MCV.isnull() & PLT.isnull() & RBC.isnull())",
+    comp_data = data.query("~(f__WBC.isnull() & f__HGB.isnull() & f__MCV.isnull() & f__PLT.isnull() & f__RBC.isnull())",
                            engine='python')
     return len(comp_data)
