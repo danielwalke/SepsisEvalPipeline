deec53e149ffdb29690a72f499c17981ff05af1c
danielwalke <daniel.walke@ovgu.de>
2025-12-30 08:53:05 +0100

    powreshell script for easier mimic preprocessing

--- 0_mimic_preprocess/preprocess_mimic.ps1
+++ 0_mimic_preprocess/preprocess_mimic.ps1
@@ -1,5 +1,5 @@
 docker run --rm `
   -v "D:\Datasets\mimic-iv-2.2:/app/input" `
-  -v "${PWD}/preprocessed_file:/app/output" `
-  -v "${PWD}/features:/app/features" `
+  -v "${PWD}/0_mimic_preprocess/preprocessed_file:/app/output" `
+  -v "${PWD}/0_mimic_preprocess/features:/app/features" `
   mimic-preprocessor
