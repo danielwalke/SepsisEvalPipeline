101b8f207d25a80233b26c82e95edaa90eed3673
Daniel Walke <daniel.walke@ovgu.de>
2026-04-07 09:21:07 +0200

    fixed bug in shell script to  use  current PWD

--- 0_mimic_preprocess/preprocess_mimic.sh
+++ 0_mimic_preprocess/preprocess_mimic.sh
@@ -1,5 +1,5 @@
 docker run --rm \
-  -v "/Users/danielwalke/git/SepsisEvalPipeline/mimic:/app/input" \
+  -v "${PWD}/mimic:/app/input" \
   -v "${PWD}/0_mimic_preprocess/preprocessed_file:/app/output" \
   -v "${PWD}/0_mimic_preprocess/features:/app/features" \
   mimic-preprocessor
