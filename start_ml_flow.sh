eafbe83583bd1b40ee7c593101db87a90d100f59
Daniel Walke <daniel.walke@ovgu.de>
2026-04-14 08:39:16 +0200

    mlflow container start with persistent db

--- start_ml_flow.sh
+++ start_ml_flow.sh
@@ -1,1 +1,9 @@
-docker run -d -p 5000:5000 --name mlflow_container ghcr.io/mlflow/mlflow:latest mlflow server --host 0.0.0.0 --allowed-hosts "host.docker.internal:5000,host.docker.internal,localhost,localhost:5000,127.0.0.1,127.0.0.1:5000"
+docker run -d -p 5000:5000 \
+  --name mlflow_container \
+  -v $(pwd)/mlflow_data:/mlflow \
+  ghcr.io/mlflow/mlflow:latest \
+  mlflow server \
+  --host 0.0.0.0 \
+  --allowed-hosts "host.docker.internal:5000,host.docker.internal,localhost,localhost:5000,127.0.0.1,127.0.0.1:5000" \
+  --backend-store-uri sqlite:////mlflow/mlflow.db \
+  --default-artifact-root /mlflow/artifacts
