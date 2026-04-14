d3302d0b8828cbb6009c1db931220b0d3b33da97
Daniel Walke <daniel.walke@ovgu.de>
2026-04-14 09:12:00 +0200

    mlflow container with allowed host for access

--- start_ml_flow.sh
+++ start_ml_flow.sh
@@ -1,9 +1,12 @@
+mkdir -p $(pwd)/mlflow_data/artifacts
+chmod 777 $(pwd)/mlflow_data
+
 docker run -d -p 5000:5000 \
   --name mlflow_container \
   -v $(pwd)/mlflow_data:/mlflow \
   ghcr.io/mlflow/mlflow:latest \
   mlflow server \
   --host 0.0.0.0 \
-  --allowed-hosts "host.docker.internal:5000,host.docker.internal,localhost,localhost:5000,127.0.0.1,127.0.0.1:5000" \
+  --allowed-hosts "*" \
   --backend-store-uri sqlite:////mlflow/mlflow.db \
   --default-artifact-root /mlflow/artifacts
